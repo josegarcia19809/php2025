2bb1079e00b55bbdfcfba735b7d0c20e0ba7741a
Jose Garcia <josegarcia19809@gmail.com>
2025-03-12 16:13:39 -0600

    PHP - Sintaxis y Funciones: Variables y Constantes en PHP

--- 02-variables.php
+++ 02-variables.php
@@ -1,6 +1,15 @@
 <?php include 'includes/header.php';
 
+$nombre = "José";
 
+echo $nombre;
+echo '<br>';
+
+var_dump($nombre);
+echo '<br>';
+
+const precio = 2000;
+echo precio;
 
 
 include 'includes/footer.php';
