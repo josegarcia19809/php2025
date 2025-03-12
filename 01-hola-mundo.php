27763716ebbd2ddce8ebb9adbe18b7981134367e
Jose Garcia <josegarcia19809@gmail.com>
2025-03-12 16:03:14 -0600

    PHP - Sintaxis y Funciones: Hola Mundo en PHP

---
+++ 01-hola-mundo.php
@@ -0,0 +1,14 @@
+<?php include 'includes/header.php';
+
+echo "<h1>Hola mundo, bienvenido a PHP</h1>";
+var_dump("Bienvenido");
+echo "<br>";
+var_dump(20);
+
+echo "<br>";
+echo "php -S localhost:3000";
+echo "<br>";
+echo "http://localhost:3000/01-hola-mundo.php";
+
+
+include 'includes/footer.php';
