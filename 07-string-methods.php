62faefff67913b89ceb4f68aa518f58dfd52229d
Jose Garcia <josegarcia19809@gmail.com>
2025-03-15 18:22:58 -0600

    PHP - Sintaxis y Funciones: Funciones para Strings en PHP

--- 07-string-methods.php
+++ 07-string-methods.php
@@ -1,6 +1,37 @@
-<?php include 'includes/header.php';
+<?php
 
+function imprimirLinea(): void
+{
+    echo PHP_EOL;
+}
 
+$nombreCliente = "   José   ";
+echo strlen($nombreCliente);
+imprimirLinea();
+echo $nombreCliente;
+imprimirLinea();
+$nombreCliente = trim($nombreCliente);
+echo $nombreCliente;
+imprimirLinea();
+echo strtoupper($nombreCliente);
+imprimirLinea();
+echo strtolower($nombreCliente);
+imprimirLinea();
 
+$nombreCliente = "José Luis";
+echo str_replace("José", "J", $nombreCliente);
+imprimirLinea();
 
-include 'includes/footer.php';
+// Revisar si un string existe o no
+echo strpos($nombreCliente, "Luis");
+imprimirLinea();
+
+$tipoCliente= "Premium";
+echo "El cliente ". $nombreCliente ." es ". $tipoCliente;
+imprimirLinea();
+echo "El cliente {$nombreCliente} es {$tipoCliente}";
+imprimirLinea();
+
+echo "Introduce tu nombre: ";
+$nombre = trim(fgets(STDIN)); // `trim()` elimina espacios en blanco y saltos de línea
+echo "Hola, $nombre!";
