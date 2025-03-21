c2a7fbd59247cab4c2ee6e35c6f3a0b56d149f8f
Jose Garcia <josegarcia19809@gmail.com>
2025-03-21 07:35:43 -0600

    PHP - Sintaxis y Funciones: Arreglos asociativos o Associative Arrays

--- 09-arrays-assoc.php
+++ 09-arrays-assoc.php
@@ -1,6 +1,27 @@
-<?php include 'includes/header.php';
+<?php
+function imprimirLinea(): void
+{
+    echo PHP_EOL;
+}
 
+$cliente = [
+    'nombre' => 'Juan',
+    'saldo' => 100,
+    'información' => [
+        'tipo' => 'premium',
+    ]
+];
 
+var_dump($cliente);
 
+imprimirLinea();
+echo $cliente['nombre'];
+imprimirLinea();
+echo $cliente['saldo'];
+imprimirLinea();
+echo $cliente['información']['tipo'];
+imprimirLinea();
 
-include 'includes/footer.php';
+// Agregar un nuevo elemento
+$cliente['codigo']=1234567;
+echo $cliente['codigo'];
