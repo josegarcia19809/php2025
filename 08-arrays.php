183ef222041b4f903120022a74886524a9f0d615
Jose Garcia <josegarcia19809@gmail.com>
2025-03-21 07:27:30 -0600

    PHP - Sintaxis y Funciones: Arreglos y Funciones para Arreglos en PHP

--- 08-arrays.php
+++ 08-arrays.php
@@ -1,6 +1,30 @@
-<?php include 'includes/header.php';
+<?php
+
+function imprimirLinea(): void
+{
+    echo PHP_EOL;
+}
+
+$carrito = ['Tablet', 'Televisión', 'Computadora'];
+var_dump($carrito);
+imprimirLinea();
+echo "Elemento 1 del carrito: ". $carrito[1];
+imprimirLinea();
+$carrito[3] = 'Celular';
+var_dump($carrito);
+imprimirLinea();
+
+// Añadir al final
+$carrito[] = 'Audifonos';
+var_dump($carrito);
+imprimirLinea();
+
+// Añadir al inicio
+array_unshift($carrito, 'Smartwatch');
+var_dump($carrito);
+imprimirLinea();
 
 
-
-
-include 'includes/footer.php';
+// Otra forma
+$clientes = array('Maria', 'Rox', 'José');
+var_dump($clientes);
