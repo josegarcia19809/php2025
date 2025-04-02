7954194fe8f0f79e5bf7b08538206a1fe7cef1da
Jose Garcia <josegarcia19809@gmail.com>
2025-04-01 20:58:54 -0600

    PHP - Sintaxis y Funciones: isset() y empty()

--- 10-isset-empty.php
+++ 10-isset-empty.php
@@ -1,6 +1,31 @@
-<?php include 'includes/header.php';
+<?php
 
+function imprimirLinea()
+{
+    echo PHP_EOL;
+}
 
+$clientes = [];
+$clientes2 = array();
+$clientes3 = array("Carolina", "Rox", "Julian");
 
+$cliente = [
+    'nombre' => 'Carolina',
+    'saldo' => 200
+];
 
-include 'includes/footer.php';
+var_dump(empty($clientes));
+var_dump(empty($clientes2));
+var_dump(empty($clientes3));
+
+imprimirLinea();
+
+// Isset: revisar si un arreglo está creado o si una propiedad está definida
+var_dump(isset($clientes4));
+var_dump(isset($clientes3));
+
+echo "Existe la propiedad 'nombre' en cliente: ";
+var_dump(isset($cliente['nombre']));
+
+echo "Existe la propiedad 'codigo' en cliente: ";
+var_dump(isset($cliente['codigo']));
