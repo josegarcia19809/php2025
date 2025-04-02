d04c69c22c36736e07e4910a8429a73417a49038
Jose Garcia <josegarcia19809@gmail.com>
2025-04-01 21:44:24 -0600

    PHP - Sintaxis y Funciones: Revisar si un elemento existe en un array y otros métodos

--- 11-funciones-arrays.php
+++ 11-funciones-arrays.php
@@ -1,7 +1,54 @@
-<?php include 'includes/header.php';
+<?php
 
+function imprimirLinea()
+{
+    echo PHP_EOL;
+}
 
+// in_array: buscar elementos en un arreglo
+$carrito = ["Tablet", "Computadora", "Televisión"];
 
+echo "Existe 'Tablet' en 'carrito': ";
+var_dump(in_array("Tablet", $carrito));
 
+echo "Existe 'Audifonos' en 'carrito': ";
+var_dump(in_array("Audifonos", $carrito));
 
-include 'includes/footer.php';
+// Ordenar elementos de un arreglo
+$numeros = array(7, 6, 5, 4, 3);
+sort($numeros);
+echo implode(", ", $numeros);
+imprimirLinea();
+
+rsort($numeros); // Descendente
+echo implode(", ", $numeros);
+imprimirLinea();
+
+// Ordenar arreglo asociativo
+$cliente = array(
+    'saldo' => 200,
+    'tipo' => 'Premium',
+    'nombre' => 'Carolina'
+);
+
+imprimirLinea();
+asort($cliente); // Ordena por valores
+echo "Ordenado por valores:\n";
+foreach ($cliente as $key => $value) {
+    echo $key . " " . $value . "\n";
+}
+
+imprimirLinea();
+ksort($cliente);
+echo "Ordenado por llaves:\n";
+foreach ($cliente as $key => $value) {
+    echo $key . " " . $value . "\n";
+}
+
+imprimirLinea();
+krsort($cliente); // En orden inverso
+// En una sola línea
+echo "Ordenado por llaves en orden inverso: " . json_encode($cliente) . "\n";
+
+arsort($cliente);
+echo "Ordenado por valores en orden inverso: " . json_encode($cliente) . "\n";
