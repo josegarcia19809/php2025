8b7f278f6ef0feabf243506f7c4767a98be3bf91
Jose Garcia <josegarcia19809@gmail.com>
2025-04-06 07:01:43 -0600

    PHP - Sintaxis y Funciones: json_encode y json_decode

--- 18.json.php
+++ 18.json.php
@@ -1,8 +1,36 @@
-<?php include 'includes/header.php';
+<?php
 
+$productos = [
+    [
+        "nombre" => "Tablet",
+        "precio" => 200,
+        "disponible" => true
+    ],
+    [
+        "nombre" => "Laptop",
+        "precio" => 750,
+        "disponible" => true
+    ],
+    [
+        "nombre" => "Smartphone",
+        "precio" => 500,
+        "disponible" => false
+    ],
+    [
+        "nombre" => "Monitor",
+        "precio" => 300,
+        "disponible" => true
+    ],
+    [
+        "nombre" => "Teclado",
+        "precio" => 50,
+        "disponible" => false
+    ]
+];
 
+$json = json_encode($productos, JSON_UNESCAPED_UNICODE);
+echo $json;
 
-
-
-
-include 'includes/footer.php';
+$json_to_array = json_decode($json);
+echo "\n";
+echo var_dump($json_to_array);
