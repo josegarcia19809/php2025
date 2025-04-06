fa64c9586ad18b0606a0b59fc979a421297af5f7
Jose Garcia <josegarcia19809@gmail.com>
2025-04-06 06:53:39 -0600

    PHP - Sintaxis y Funciones: Include y require

--- 17-require.php
+++ 17-require.php
@@ -1,8 +1,12 @@
-<?php include 'includes/header.php';
+<?php require 'includes/header.php';
 
+echo "<h1>Página con include y require</h1>";
 
+require_once "15-funciones.php";
 
-
+//saludar(); // Imprime: Hola, Invitado. ¡Bienvenido!
+//saludar("Ana"); // Imprime: Hola, Ana. ¡Bienvenido!
+//saludar("Luis", "¡Qué bueno verte!"); // Imprime: Hola, Luis. ¡Qué bueno verte!
 
 
 include 'includes/footer.php';
