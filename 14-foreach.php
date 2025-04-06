42b0e2ef96d7944af3043838b80a5f797e3b7bd7
Jose Garcia <josegarcia19809@gmail.com>
2025-04-05 21:05:15 -0600

    PHP - Sintaxis y Funciones: foreach y mostrar el contenido de un array

--- 14-foreach.php
+++ 14-foreach.php
@@ -1,6 +1,82 @@
-<?php include 'includes/header.php';
+<?php
 
+function imprimirLinea()
+{
+    echo PHP_EOL;
+}
 
+$clientes = array("Pedro", "María", "Carolina");
 
+foreach ($clientes as $cliente) {
+    echo "$cliente\n";
+}
 
-include 'includes/footer.php';
+imprimirLinea();
+
+for ($i = 0; $i < count($clientes); $i++) {
+    echo "$clientes[$i]\n";
+}
+
+imprimirLinea();
+
+$cliente = [
+    "nombre" => "Juan",
+    "saldo" => 200,
+    "tipo" => "Premium"
+];
+
+foreach ($cliente as $key => $value) {
+    echo "$key: $value\n";
+}
+
+imprimirLinea();
+
+$productos = [
+    [
+        "nombre" => "Tablet",
+        "precio" => 200,
+        "disponible" => true
+    ],
+    [
+        "nombre" => "Laptop",
+        "precio" => 750,
+        "disponible" => true
+    ],
+    [
+        "nombre" => "Smartphone",
+        "precio" => 500,
+        "disponible" => false
+    ],
+    [
+        "nombre" => "Monitor",
+        "precio" => 300,
+        "disponible" => true
+    ],
+    [
+        "nombre" => "Teclado",
+        "precio" => 50,
+        "disponible" => false
+    ]
+];
+
+foreach ($productos as $producto) {
+    echo var_dump($producto);
+}
+
+imprimirLinea();
+
+foreach ($productos as $producto) {
+    echo "Producto: " . $producto['nombre'] . "\n";
+    echo "Precio: " . $producto['precio'] . "\n";
+    if ($producto['disponible']) {
+        echo "El producto está disponible" . "\n";
+    } else {
+        echo "No Disponible\n";
+    }
+
+    // Operador ternario
+    echo ($producto['disponible']) ? "Si hay" : "No hay";
+    imprimirLinea();
+    echo str_repeat("-", 100);
+    imprimirLinea();
+}
